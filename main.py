fcabb95bc76227eaac71e101dc11a811cf71d46b
Amk1400 <kavoshamirmohammad@gmail.com>
2025-10-17 09:54:30 +0330

    init: create initial structure

---
+++ main.py
@@ -0,0 +1,6 @@
+import menu_manager
+from managers import ProjectManager
+
+if __name__ == "__main__":
+    project_manager = ProjectManager()
+    menu_manager.main_menu(project_manager)
---
+++ menu_manager.py
@@ -0,0 +1,6 @@
+def main_menu(project_manager):
+    return None
+
+
+def projects_menu(project_manager):
+    pass

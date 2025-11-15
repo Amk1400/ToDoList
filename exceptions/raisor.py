9725f11547cd68c903ebfd2870d327298ecb0589
Amk1400 <kavoshamirmohammad@gmail.com>
2025-11-15 19:39:58 +0330

    refactor: initialize TODOs of exceptions module

---
+++ exceptions/raisor.py
@@ -0,0 +1,8 @@
+
+"""
+TODO
+define an exception which inherits exception
+InvalidOptionException
+this exception happens when u are in a menu & your input is invalid
+for example there are 5 options in menu and you choose 6 and so on...
+"""
---
+++ exceptions/validator.py
@@ -0,0 +1,10 @@
+"""
+TODO
+define an validator which its input argument is min & max
+all natural numbers in that range is valid
+in all validators in this file, we validate, then
+if it was valid return the choosn value else we raise exception
+
+all menus that fetch option from user must use this validator
+so needed to update all codes of cli layer
+"""
